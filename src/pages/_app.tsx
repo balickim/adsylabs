931ffa1c1e89e4f37bda6877c4cdf49b78d15f8b
balickim <stiekerosiem@gmail.com>
2023-04-05 17:49:24 +0200

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppType } from 'next/dist/shared/lib/utils';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import 'tailwindcss/tailwind.css';
+
+const _App: AppType = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Nextjs Tailwind CSS Styled Components with TypeScript</title>
+        <meta
+          name="description"
+          content="Nextjs Tailwind CSS Styled Components with TypeScript"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default _App;
