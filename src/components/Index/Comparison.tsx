a9eb64f6f05976544244c161650e1758ebc6c3e5
balickim <stiekerosiem@gmail.com>
2023-04-20 21:20:42 +0200

    fix comparison section

--- src/components/Index/Comparison.tsx
+++ src/components/Index/Comparison.tsx
@@ -18,7 +18,7 @@ const ColumnAsset = tw.div`flex justify-center items-center`;
 const ColumnText = tw.p`flex justify-center items-center`;
 const LeftColumnText = tw.p`flex items-center text-sm xl:text-base`;
 const Grid = styled.div`
-  ${tw`grid gap-x-4 h-[34rem] grid-rows-6 overflow-x-auto justify-center`}
+  ${tw`grid gap-x-4 h-[34rem] grid-rows-6 overflow-x-auto`}
   grid-template-columns:
     minmax(8rem, 10rem)
     minmax(11rem, 15rem) 
@@ -37,108 +37,110 @@ const Comparison = () => {
           AdsBridge vs tradycyjne rozwiązania
       </Title>
 
-      <Grid>
-        <div></div>
-        <ColumnAsset className={'border-primary border-l-4 border-t-4 border-r-4 rounded-t-xl'}>
-          <Image
-            src={`${STATIC.LOGO}`}
-            alt="AdsBridge Logo"
-            width={100}
-            height={50}
-          />
-        </ColumnAsset>
-        <HeaderContainer>
-          <p>Zewnętrzna firma HR</p>
-          <PinkText>Jak rzut kością</PinkText>
-        </HeaderContainer>
-        <HeaderContainer>
-          <p>Rekrutacja na własną rękę</p>
-          <PinkText>Długi proces, różny efekt</PinkText>
-        </HeaderContainer>
-        <HeaderContainer>
-          <p>Podobna usługa</p>
-          <PinkText>Długi proces, różny efekt</PinkText>
-        </HeaderContainer>
+      <div className={'flex justify-center'}>
+        <Grid>
+          <div></div>
+          <ColumnAsset className={'border-primary border-l-4 border-t-4 border-r-4 rounded-t-xl'}>
+            <Image
+              src={`${STATIC.LOGO}`}
+              alt="AdsBridge Logo"
+              width={100}
+              height={50}
+            />
+          </ColumnAsset>
+          <HeaderContainer>
+            <p>Zewnętrzna firma HR</p>
+            <PinkText>Jak rzut kością</PinkText>
+          </HeaderContainer>
+          <HeaderContainer>
+            <p>Rekrutacja na własną rękę</p>
+            <PinkText>Długi proces, różny efekt</PinkText>
+          </HeaderContainer>
+          <HeaderContainer>
+            <p>Podobna usługa</p>
+            <PinkText>Długi proces, różny efekt</PinkText>
+          </HeaderContainer>
 
-        <LeftColumnText>
+          <LeftColumnText>
           Gwarancja efektów kampanii lub zwrot pieniędzy za usługę
-        </LeftColumnText>
-        <ColumnAsset className={'border-primary border-l-4 border-r-4'}>
-          <TfiCheck size={ICON_SIZE} color={twConfig?.theme?.colors?.primary as string} />
-        </ColumnAsset>
-        <ColumnAsset>
-          <CgClose size={ICON_SIZE} />
-        </ColumnAsset>
-        <ColumnAsset>
-          <CgClose size={ICON_SIZE} />
-        </ColumnAsset>
-        <ColumnAsset>
-          <CgClose size={ICON_SIZE} />
-        </ColumnAsset>
+          </LeftColumnText>
+          <ColumnAsset className={'border-primary border-l-4 border-r-4'}>
+            <TfiCheck size={ICON_SIZE} color={twConfig?.theme?.colors?.primary as string} />
+          </ColumnAsset>
+          <ColumnAsset>
+            <CgClose size={ICON_SIZE} />
+          </ColumnAsset>
+          <ColumnAsset>
+            <CgClose size={ICON_SIZE} />
+          </ColumnAsset>
+          <ColumnAsset>
+            <CgClose size={ICON_SIZE} />
+          </ColumnAsset>
 
-        <LeftColumnText>
+          <LeftColumnText>
           Czas zatrudnienia specjalisty
-        </LeftColumnText>
-        <ColumnAsset className={'border-primary border-l-4 border-r-4'}>
+          </LeftColumnText>
+          <ColumnAsset className={'border-primary border-l-4 border-r-4'}>
           3-4 dni
-        </ColumnAsset>
-        <ColumnText>
+          </ColumnAsset>
+          <ColumnText>
           kilka tygodni
-        </ColumnText>
-        <ColumnText>
+          </ColumnText>
+          <ColumnText>
           nawet do 8 tygodni
-        </ColumnText>
-        <ColumnText>
+          </ColumnText>
+          <ColumnText>
           3-4 dni
-        </ColumnText>
+          </ColumnText>
 
-        <LeftColumnText>
+          <LeftColumnText>
           Dedykowany ekspert strategii marketingowej
-        </LeftColumnText>
-        <ColumnAsset className={'border-primary border-l-4 border-r-4'}>
-          <TfiCheck size={ICON_SIZE} color={twConfig?.theme?.colors?.primary as string} />
-        </ColumnAsset>
-        <ColumnAsset>
-          <CgClose size={ICON_SIZE} />
-        </ColumnAsset>
-        <ColumnAsset>
-          <CgClose size={ICON_SIZE} />
-        </ColumnAsset>
-        <ColumnAsset>
-          <CgClose size={ICON_SIZE} />
-        </ColumnAsset>
+          </LeftColumnText>
+          <ColumnAsset className={'border-primary border-l-4 border-r-4'}>
+            <TfiCheck size={ICON_SIZE} color={twConfig?.theme?.colors?.primary as string} />
+          </ColumnAsset>
+          <ColumnAsset>
+            <CgClose size={ICON_SIZE} />
+          </ColumnAsset>
+          <ColumnAsset>
+            <CgClose size={ICON_SIZE} />
+          </ColumnAsset>
+          <ColumnAsset>
+            <CgClose size={ICON_SIZE} />
+          </ColumnAsset>
 
-        <LeftColumnText>
+          <LeftColumnText>
           Cena
-        </LeftColumnText>
-        <ColumnText className={'border-primary border-l-4 border-r-4 '}>
+          </LeftColumnText>
+          <ColumnText className={'border-primary border-l-4 border-r-4 '}>
           $$
-        </ColumnText>
-        <ColumnText>
+          </ColumnText>
+          <ColumnText>
           $$$
-        </ColumnText>
-        <ColumnText>
+          </ColumnText>
+          <ColumnText>
           $$$
-        </ColumnText>
-        <ColumnText>
+          </ColumnText>
+          <ColumnText>
           $
-        </ColumnText>
+          </ColumnText>
 
-        <div></div>
-        <ColumnAsset className={'border-primary border-l-4 border-r-4 border-b-4 rounded-b-xl'}>
-          <Link href="/#pricing">
-            <StyledCtaButton
-              version={'primary'}
-              type="button"
-            >
+          <div></div>
+          <ColumnAsset className={'border-primary border-l-4 border-r-4 border-b-4 rounded-b-xl'}>
+            <Link href="/#pricing">
+              <StyledCtaButton
+                version={'primary'}
+                type="button"
+              >
               Znajdź specjalistę
-            </StyledCtaButton>
-          </Link>
-        </ColumnAsset>
-        <div></div>
-        <div></div>
-        <div></div>
-      </Grid>
+              </StyledCtaButton>
+            </Link>
+          </ColumnAsset>
+          <div></div>
+          <div></div>
+          <div></div>
+        </Grid>
+      </div>
     </StyledSection>
   );
 };
