da8f46a90febe17c9231f7779e6cd1013814a11c
balickim <stiekerosiem@gmail.com>
2023-08-13 19:48:04 +0200

    AD-0: fix footer

--- src/components/Index/Footer.tsx
+++ src/components/Index/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer () {
   const { pathname } = useRouter();
 
   return (
-    <footer className={'flex gap-x-6 justify-center my-10'}>
+    <footer className={'flex flex-col items-center gap-6 justify-center my-10 lg:flex-row'}>
       <a className={'text-[#C8C7D8]'} href={'./join-us'}>Dołącz jako specjalista</a>
       <a className={'text-[#C8C7D8]'} href={pathname.includes('join-us') ? '/join-us/faq' : '/faq'}>FAQ</a>
       <a className={'text-[#C8C7D8]'} href={'./polityka-prywatnosci'}>Polityka prywatności</a>
